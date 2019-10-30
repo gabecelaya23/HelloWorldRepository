97a5b4e64d68cab394a64bf2d8bf0ec1a909eefd
gabecelaya23 <gabecelaya23@gmail.com>
2019-10-30 09:58:26 -0700

    Sum App

---
+++ HelloWorld/src/GabeSumApp.java
@@ -0,0 +1,19 @@
+
+public class GabeSumApp {
+
+	public static void main(String[] args) {
+		int a = 0, b = 0;
+		
+		System.out.println(sum(a, b));
+
+	}
+	
+	public static int sum(int a, int b) {
+		int sum;
+		a = 5;
+		b = 12;
+		
+		return sum = a + b;
+	}
+
+}
